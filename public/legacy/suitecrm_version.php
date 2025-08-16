6072442bf802252e73b5062d2bcc83a1327fe01b
sarapil <mataz.sarapil@gmail.com>
2025-08-16 15:12:27 +0300

    first commit

---
+++ public/legacy/suitecrm_version.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$suitecrm_version = '8.8.1';
+$suitecrm_timestamp = '2025-08-06 12:00:00';
+$suitecrm_legacy = '7.14.7';
